bb807a3626bde404547d8dbdd32f94eebb9b7e75
baka <abhishek>
2023-10-22 22:08:46 +0530

    Added the code for terabox api and bot.

---
+++ main.py
@@ -0,0 +1,53 @@
+from download import fetch_details, downloader, get_formatted_size
+from pyrogram import Client, filters
+
+app = Client("teraBox")
+
+
+@app.on_message(filters.command("start"))
+def start(client, message):
+    message.reply_text("Hello! I'm Terabox link Bypass Bot. Send me a link to download from Terabox.", quote=True)
+
+
+@app.on_message(filters.command("bypass"))
+def bypass(client, message):
+    try:
+        url = message.text.split(" ", 1)[1]
+        files = fetch_details(url)
+        if files:
+            video_links = list()
+            data = files
+            for item in data['list']:
+                if item['is_dir'] == '0':
+                    fs_id = item['fs_id']
+                    timestamp = data['timestamp']
+                    sign = data['sign']
+                    share_id = data['shareid']
+                    uk = data['uk']
+                    download_link = downloader(share_id, uk, sign, timestamp, fs_id)
+                    video_links.append(
+                        {'name': item['filename'], 'size': get_formatted_size(item['size']), 'link': download_link})
+                elif item['is_dir'] == '1':
+                    for child_item in item['children']:
+                        fs_id = child_item['fs_id']
+                        timestamp = data['timestamp']
+                        sign = data['sign']
+                        share_id = data['shareid']
+                        uk = data['uk']
+                        download_link = downloader(fs_id, timestamp, sign, share_id, uk)
+                        video_links.append(
+                            {'name': child_item['filename'], 'size': get_formatted_size(child_item['size']),
+                             'link': download_link})
+                # format the links to send
+                text = ""
+                for link in video_links:
+                    text += f"Name 📹: {link['name']}\nSize 📏: {link['size']}\nDLink 📥: {link['link']}\n"
+                message.reply_text(text, quote=True)
+        else:
+            message.reply_text("Invalid URL", quote=True)
+    except IndexError as e:
+        message.reply_text("Enter the URL", quote=True)
+
+
+# run the application
+app.run()
