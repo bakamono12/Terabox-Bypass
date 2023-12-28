d9ec81abb83079f30d52e2c199d583c1dd4215ef
baka <abhishek>
2023-12-29 02:41:31 +0530

    Added the config file for configuration of the bot

---
+++ downloader.py
@@ -0,0 +1,112 @@
+import aiohttp
+import requests
+import re
+from config import my_cookie, my_headers
+
+# set the environment vars for headers and cookies
+my_session = aiohttp.ClientSession(cookies=my_cookie)
+my_session.headers.update(my_headers)
+
+
+def get_formatted_size(size_bytes):
+    try:
+        size_bytes = int(size_bytes)
+        size = size_bytes / (1024 * 1024) if size_bytes >= 1024 * 1024 else (
+            size_bytes / 1024 if size_bytes >= 1024 else size_bytes
+        )
+        unit = "MB" if size_bytes >= 1024 * 1024 else ("KB" if size_bytes >= 1024 else "bytes")
+
+        return f"{size:.2f} {unit}"
+    except Exception as e:
+        print(f"Error getting formatted size: {e}")
+        return None
+
+
+def is_valid_url(url):
+    try:
+        _ = my_session.get(url)
+        return True
+    except Exception as e:
+        return False
+
+
+def check_url_patterns(url):
+    patterns = [
+        r"ww\.mirrobox\.com",
+        r"www\.nephobox\.com",
+        r"freeterabox\.com",
+        r"www\.freeterabox\.com",
+        r"1024tera\.com",
+        r"4funbox\.co",
+        r"www\.4funbox\.com",
+        r"mirrobox\.com",
+        r"nephobox\.com",
+        r"terabox\.app",
+        r"terabox\.com",
+        r"www\.terabox\.ap",
+        r"terabox\.fun",
+        r"www\.terabox\.com",
+        r"www\.1024tera\.co",
+        r"www\.momerybox\.com",
+        r"teraboxapp\.com",
+        r"momerybox\.com",
+        r"tibibox\.com",
+        r"www\.tibibox\.com",
+        r"www\.teraboxapp\.com",
+    ]
+
+    if not is_valid_url(url):
+        return False
+
+    for pattern in patterns:
+        if re.search(pattern, url):
+            return True
+    return False
+
+
+async def find_between(string, start, end):
+    start_index = string.find(start) + len(start)
+    end_index = string.find(end, start_index)
+    return string[start_index:end_index]
+
+
+async def fetch_download_link_async(url):
+    try:
+        async with my_session.get(url) as response:
+            response.raise_for_status()
+            response_data = await response.text()
+
+            js_token = await find_between(response_data, 'fn%28%22', '%22%29')
+            log_id = await find_between(response_data, 'dp-logid=', '&')
+
+            if not js_token or not log_id:
+                return None
+
+            request_url = str(response.url)
+            surl = request_url.split('surl=')[1]
+            params = {
+                'app_id': '250528',
+                'web': '1',
+                'channel': 'dubox',
+                'clienttype': '0',
+                'jsToken': js_token,
+                'dplogid': log_id,
+                'page': '1',
+                'num': '20',
+                'order': 'time',
+                'desc': '1',
+                'site_referer': request_url,
+                'shorturl': surl,
+                'root': '1'
+            }
+
+            async with my_session.get('https://www.terabox.app/share/list', params=params) as response2:
+                response_data2 = await response2.json()
+                if 'list' not in response_data2:
+                    return None
+                return response_data2['list']
+
+    except aiohttp.ClientResponseError as e:
+        print(f"Error fetching download link: {e}")
+        return None
+
