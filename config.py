5c6b736830d1c7cd8b862b76fc2292cf982dc361
baka <abhishek>
2024-01-01 00:23:47 +0530

    added aria2c downloader.

--- config.py
+++ config.py
@@ -3,13 +3,15 @@ import logging
 import re
 
 logger = logging.getLogger(__name__)
-
 my_cookie = os.environ.get("MY_COOKIES")
 my_headers = os.environ.get("MY_HEADERS")
 session_string = os.environ.get("session_string")
 allowed_groups = os.environ.get("allowed_groups") or "-123232ZCVZB"  # added random group id to avoid NoneType error
 # allowed_groups = ["-123232ZCVZB"] or ["121222xxx", "123456xxx"]
 owner_id = os.environ.get("owner_id") or ""
+PATH = "./downloads"
+EMOJI = ["😀", "😃", "😄", "😁", "😆", "😅", "😂", "🤣", "😊", "😇", "🙂", "🙃", "😉", "😌", "😍", "🥰", "😘",
+         "😗", "😙", "😚", ]
 
 try:
     my_cookie = eval(my_cookie)
@@ -24,8 +26,12 @@ def extract_links(message):
     try:
         url_pattern = r'https?://\S+'
         matches = re.findall(url_pattern, message)
-
         return matches
     except Exception as e:
         logger.error(f"Error extracting links: {e}")
         return []
+
+
+async def extract_gid(message_update):
+    gid = [item.split(':')[1].strip() for item in message_update.split('\n') if 'GID' in item or 'Name' in item]
+    return gid if gid else None
--- main.py
+++ main.py
@@ -1,12 +1,18 @@
+import asyncio
+import os
+import random
 import time
 import logging
 from pyrogram import Client, filters, enums
-from config import session_string, allowed_groups, owner_id, extract_links
-from downloader import check_url_patterns_async, fetch_download_link_async, get_formatted_size_async
+from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
+from config import session_string, allowed_groups, owner_id, extract_links, extract_gid, EMOJI, PATH
+from downloader import check_url_patterns_async, fetch_download_link_async, get_formatted_size_async, Aria2Downloader
 
 app = Client("teraBox", session_string=session_string)
-logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
 
+# initialize downloader
+downloader = Aria2Downloader()
 
 @app.on_message(filters.command("start"))
 async def start(client, message):
@@ -56,28 +62,86 @@ async def link_handler(client, message):
             await message.reply_text("⚠️ No valid URLs found!", quote=True)
             return
         try:
+            buttons = [
+                InlineKeyboardButton("Download", callback_data="download"),
+            ]
             for url in urls:
                 if not await check_url_patterns_async(url):
                     await client.send_chat_action(message.chat.id, enums.ChatAction.TYPING)
-                    await message.reply_text("⚠️ Not a valid Terabox URL!", quote=True)
+                    await message.reply_text("⚠️ No valid Terabox URLs Found!", quote=True)
                     continue
-
                 await client.send_chat_action(message.chat.id, enums.ChatAction.TYPING)
                 process_url = await message.reply_text("🔎 Processing URL...", quote=True)
-                link_data = await fetch_download_link_async(url)
-
                 end_time = time.time()
+                link_data = await fetch_download_link_async(url)
                 time_taken = end_time - start_time
                 await client.send_chat_action(message.chat.id, enums.ChatAction.TYPING)
-
                 link_message = "\n\n".join([await format_message(link) for link in link_data])
                 download_message = (
                     f"🔗 <b>Link Bypassed!</b>\n\n{link_message}\n\n<b>Time Taken</b>: {time_taken:.2f} seconds"
                 )
-                await process_url.edit_text(download_message)
+                await process_url.edit_text(download_message, reply_markup=InlineKeyboardMarkup([buttons]),
+                                            disable_web_page_preview=True)
 
         except Exception as e:
             await message.reply_text(f"Error: {e}", quote=True)
+
+
+@app.on_callback_query(filters.regex(r"download"))
+async def download_handler(client, callback_data):
+    url = callback_data.message.reply_to_message.text
+    # check if the link sender and the button clicker are the same
+    if callback_data.from_user.id != callback_data.message.reply_to_message.from_user.id:
+        await callback_data.answer("You didn't send this link.", show_alert=True)
+        return
+    cancel_buttons = [
+        InlineKeyboardButton("Cancel", callback_data="cancel"),
+    ]
+    # Initialize the download
+    files = downloader.start_download(url)
+    response_text = "Starting download..."
+    for file_update in files:
+        try:
+            response_text = (
+                f"Status: `{file_update['status']}`\n"
+                f"Name: `{file_update['name']}`\n"
+                f"Downloaded: `{file_update['downloaded']}`\n"
+                f"Speed: `{file_update['download_speed']}`\n"
+                f"ETA: `{file_update['eta']}`\n"
+                f"GID: `{file_update['gid']}`"
+            )
+            await callback_data.edit_message_text(response_text, reply_markup=InlineKeyboardMarkup([cancel_buttons]))
+            if file_update['is_complete']:
+                response_text = (
+                    f"Status: `{file_update['status']}`\n"
+                    f"Name: `{file_update['name']}`\n"
+                    f"Downloaded: `{file_update['downloaded']}\n`"
+                    f"GID: `{file_update['gid']}`"
+                )
+                await callback_data.edit_message_text(response_text)
+                break
+            await asyncio.sleep(3.5)
+        except Exception as e:
+            logger.error(e)
+            await asyncio.sleep(2.5)
+            await callback_data.edit_message_text(f"Some Error Occurred: {e}\n\n {random.choice(EMOJI)}")
+
+
+@app.on_callback_query(filters.regex(r"cancel"))
+async def cancel_download_handler(client, callback_data):
+    # Handle cancel button click
+    name, gid = await extract_gid(callback_data.message.text)
+    if gid:
+        try:
+            downloader.cancel_download(gid)
+            os.remove(PATH + "/" + name)
+            await callback_data.message.edit_text("Download cancelled and removed.")
+            return
+        except Exception as e:
+            logger.error(e)
+            await callback_data.message.edit_text(f"Some Error Occurred: {e}\n\n {random.choice(EMOJI)}")
+            return
+    await callback_data.message.edit_text("Download already completed/Cancelled/Removed.")
 
 
 # run the application
